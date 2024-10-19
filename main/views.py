e27e5b5f0b2cce39fb22eab554d460d9c5eaa549
AdDoha0 <adamdoxaev@gmail.com>
2024-10-19 18:01:20 +0300

    main приложение готово, страница index и about

--- main/views.py
+++ main/views.py
@@ -1,14 +1,26 @@
+from email import contentmanager
+import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    data = {
-        "title": "Home",
-        "content": "Главная страница HOME"
+
+    context = {
+        "title": "HOME - главная",
+        "content": "Магазин мебели HOME",
     }
-    return render(request, "main/index.html", data)
+
+
+    return render(request, "main/index.html", context)
 
 def about(request):
-    return HttpResponse("about page")
+
+    context = {
+        "title": "О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о том какой у нас классный и хороший товар"
+    }
+
+    return render(request, "main/about.html", context)
