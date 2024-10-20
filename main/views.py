1a06a408608e64d1f76d8afc0873b3fcec538ad3
AdDoha0 <adamdoxaev@gmail.com>
2024-10-20 20:18:21 +0300

    добавил фиктуры

--- main/views.py
+++ main/views.py
@@ -1,15 +1,16 @@
-from email import contentmanager
-import re
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
-# Create your views here.
+from goods.models import Categories
 
 def index(request):
+
+    categories = Categories.objects.all()
 
     context = {
         "title": "HOME - главная",
         "content": "Магазин мебели HOME",
+        "categories": categories
     }
 
 
