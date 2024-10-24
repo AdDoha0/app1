37b46221897a5be9671b1ecfd73c3cd006d39e3d
AdDoha0 <adamdoxaev@gmail.com>
2024-10-24 16:51:57 +0300

    первая пагинация

--- goods/views.py
+++ goods/views.py
@@ -1,27 +1,32 @@
 
+from locale import currency
+from msilib import UuidCreate
 from turtle import title
-from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template import context
+from django.views.defaults import page_not_found
 
 from goods.models import Products
 
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
 
     if category_slug == "all":
         goods =  Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
 
-
-
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods
+        "goods": current_page,
+        "slug_url": category_slug
     }
 
     return render(request, 'goods/catalog.html', context)
